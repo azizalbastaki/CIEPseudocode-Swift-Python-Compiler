d78b821d85fba91b32cb3d7c1608189f3bbea1b8
Abdulaziz <azalbastaki@live.com>
2021-07-08 22:52:32 +0400

    methods prepared

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
-
-
 class compiler():
     def __init__(self,lang):
         self.language = lang
-
+        self.scope = 0
     def compile(self,file):
         self.file = open(file,"r")
-
         for i in self.file:
             pass
+
+    def __performDiagnosis(self,line):
+        pass
+    def __checkAssignment(self,line):
+        pass
+    def __checkSelection(self,line):
+        pass
+    def __checkIteration(self,line):
+        pass
+    def __checkMethods(self,line):
+        pass
+    def __checkOperations(self,line):
+        pass
+    def __checkFile(self,line):
+        pass
+    def __checkUserDataTypes(self,line):
+        pass
