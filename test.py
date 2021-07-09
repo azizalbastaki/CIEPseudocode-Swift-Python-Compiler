e26383555113ac4f491a93a53191de6741a5090e
Abdulaziz <azalbastaki@live.com>
2021-07-10 00:37:16 +0400

    Compiler can now compile variable declarations

--- test.py
+++ test.py
@@ -4,28 +4,40 @@ class compiler():
         self.language = lang
         self.scope = 0
         self.currentLine = ""
+        print(self.checkAssignment("DECLARE SCm: STRING"))
     def compile(self,file):
         self.file = open(file,"r")
         for i in self.file:
             self.currentLine = i.strip()
-            pass
-
     def __performDiagnosis(self,line):
         pass
-    def __checkAssignment(self,line):
-        self.assignmentHolder = self.currentLine.split()
+    def checkAssignment(self,line):
+        self.assignmentHolder = line.split()
         if self.assignmentHolder[0] == "DECLARE":
-            pass
+            identifer = self.assignmentHolder[1]
+            datatype = ""
+            if identifer[-1] == ":":
+                identifer = identifer[0:-1]
+                datatype = self.assignmentHolder[2]
+            else:
+                datatype = self.assignmentHolder[3]
+            return (self.language.declareVariable(identifer, datatype))
+    def checkSelection(line):
+        pass
+    def checkIteration(line):
+        pass
+    def checkMethods(line):
+        pass
+    def checkOperations(line):
+        pass
+    def checkFile(line):
+        pass
+    def checkUserDataTypes(line):
+        pass
+from swiftCompiler import swift5Compiler
+from python3Compiler import python3Compiler
+swiftComp = swift5Compiler()
+pythonComp = python3Compiler()
 
-    def __checkSelection(self,line):
-        pass
-    def __checkIteration(self,line):
-        pass
-    def __checkMethods(self,line):
-        pass
-    def __checkOperations(self,line):
-        pass
-    def __checkFile(self,line):
-        pass
-    def __checkUserDataTypes(self,line):
-        pass
+swiftCom = compiler(swiftComp)
+pythonComp = compiler(pythonComp)
