a09ddfa3e00451967fa29cd4db4ec7c81c89f17a
Abdulaziz <azalbastaki@live.com>
2021-07-09 17:10:22 +0400

    methods prepared

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
+#   ~Abdulaziz Albastaki July 2021
 class compiler():
     def __init__(self,lang):
         self.language = lang
         self.scope = 0
+        self.currentLine = ""
     def compile(self,file):
         self.file = open(file,"r")
         for i in self.file:
+            self.currentLine = i.strip()
             pass
 
     def __performDiagnosis(self,line):
         pass
     def __checkAssignment(self,line):
-        pass
+        self.assignmentHolder = self.currentLine.split()
+        if self.assignmentHolder[0] == "DECLARE":
+            pass
+
     def __checkSelection(self,line):
         pass
     def __checkIteration(self,line):
